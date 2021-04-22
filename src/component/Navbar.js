d613506ecbaaf40ed290043bb4baa368e693fcc9
Wayne <tsun0227@yahoo.com.tw>
2021-04-22 23:36:47 +0800

    navbar RWD

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,11 +2,12 @@ import React, { useEffect } from "react";
 
 function Navbar(props) {
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      window.scrollY >= 50
-        ? document.querySelector(".navbar").classList.add("navbarFix")
-        : document.querySelector(".navbar").classList.remove("navbarFix");
-    });
+    document.body.clientWidth >= 540 &&
+      window.addEventListener("scroll", () => {
+        window.scrollY >= 50
+          ? document.querySelector(".navbar").classList.add("navbarFix")
+          : document.querySelector(".navbar").classList.remove("navbarFix");
+      });
     return () => {};
   }, []);
   return (
