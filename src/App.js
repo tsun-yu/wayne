b345cbb5383a57f793ba376e8779d19c78aeac7c
Wayne <tsun0227@yahoo.com.tw>
2021-04-23 11:03:11 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Switch } from "react-router";
 import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Switch>
+        <Route path="/" exact></Route>
+      </Switch>
     </>
   );
 }
