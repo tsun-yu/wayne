c05d7298c0e4dec9abb06eeb48b2217a40b8ba88
Wayne <tsun0227@yahoo.com.tw>
2021-05-04 22:25:09 +0800

    font adjustment

--- src/component/hompage/Banner.js
+++ src/component/hompage/Banner.js
@@ -5,8 +5,10 @@ function Banner(props) {
     <>
       <div className="banner">
         <div className="banner__cover">
-          <span className="banner__title ">It's all about</span>
-          <span className="banner__title">WAYNE HUANG</span>
+          <span className="banner__title ">
+            I am <span className="banner__title--primary">Wayne</span> Huang
+          </span>
+
           <hr />
           <div className="banner__title--sm">
             <span>Welcome To My Website</span>
