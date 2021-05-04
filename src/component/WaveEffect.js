f5f672e21dc323831a73b58563042bc57f2bf3c8
Wayne <tsun0227@yahoo.com.tw>
2021-05-04 14:43:45 +0800

    wave effect

--- src/component/WaveEffect.js
+++ src/component/WaveEffect.js
@@ -17,12 +17,10 @@ function WaveEffect(props) {
   }, []);
   return (
     <>
-      <div className="waves">
-        <div className="waves__wave" id="wave1"></div>
-        <div className="waves__wave" id="wave2"></div>
-        <div className="waves__wave" id="wave3"></div>
-        <div className="waves__wave" id="wave4"></div>
-      </div>
+      <div className="wave" id="wave1"></div>
+      <div className="wave" id="wave2"></div>
+      <div className="wave" id="wave3"></div>
+      <div className="wave" id="wave4"></div>
     </>
   );
 }
