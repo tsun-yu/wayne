e044dd5cfa091b1a74e9c5fdd72b7bc1303b5bb5
Wayne <tsun0227@yahoo.com.tw>
2021-05-09 21:34:37 +0800

    portfolios

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,15 +3,19 @@ import AboutMe from "../component/hompage/AboutMe";
 import Banner from "../component/hompage/Banner";
 import Experience from "../component/hompage/Experience";
 import Skills from "../component/hompage/Skills";
+import Works from "../component/hompage/Woks";
 
 function Homepage(props) {
   return (
     <>
       <main>
         <Banner />
-        <AboutMe />
-        <Skills />
-        <Experience />
+        <div>
+          <AboutMe />
+          <Skills />
+          <Experience />
+          <Works />
+        </div>
       </main>
     </>
   );
