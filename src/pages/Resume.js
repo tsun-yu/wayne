ce11c36b6ef1bc35960374b914d1333b1246d88c
Wayne <tsun0227@yahoo.com.tw>
2021-05-13 09:52:58 +0800

    resume fix

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,14 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Resume(props) {
-  const $cvCn = document.querySelector("#cv-cn");
-  const $cvEn = document.querySelector("#cv-en");
-  const cvToggle = (target, other) => {
-    if (target.classList.length === 1) {
-      target.classList.remove("resume__hide");
-      other.classList.add("resume__hide");
-    }
-  };
+  const [cvLink, setCvLink] = useState(
+    "https://drive.google.com/file/d/1OEz-ICghvtmSOrj9QDS95y4RSqhcmQr8/preview"
+  );
   const scrollTo = (ele) => {
     const distance =
       document.querySelector(ele).offsetTop -
@@ -19,40 +14,37 @@ function Resume(props) {
       behavior: "smooth",
     });
   };
+  const cvToggle = (a) => {
+    a === "cn"
+      ? setCvLink(
+          "https://drive.google.com/file/d/1OEz-ICghvtmSOrj9QDS95y4RSqhcmQr8/preview"
+        )
+      : setCvLink(
+          "https://drive.google.com/file/d/1XrdwWgqR4P5foeYhIFg6_fPOJD7ljfEU/preview"
+        );
+    scrollTo("#cv");
+  };
+
   return (
     <>
       <div className="resume__sec">
         <div className="resume__opt">
           <h1
             onClick={() => {
-              cvToggle($cvCn, $cvEn);
-              scrollTo("#cv-cn");
+              cvToggle("cn");
             }}
           >
             中文履歷
           </h1>
           <h1
             onClick={() => {
-              cvToggle($cvEn, $cvCn);
-              scrollTo("#cv-en");
+              cvToggle("en");
             }}
           >
             英文履歷
           </h1>
         </div>
-        <iframe
-          id="cv-cn"
-          frameBorder="0"
-          scrolling="no"
-          src="https://drive.google.com/file/d/1OEz-ICghvtmSOrj9QDS95y4RSqhcmQr8/preview"
-        ></iframe>
-        <iframe
-          className="resume__hide"
-          id="cv-en"
-          frameBorder="0"
-          scrolling="no"
-          src="https://drive.google.com/file/d/1XrdwWgqR4P5foeYhIFg6_fPOJD7ljfEU/preview"
-        ></iframe>
+        <iframe id="cv" frameBorder="0" scrolling="no" src={cvLink}></iframe>
       </div>
     </>
   );
