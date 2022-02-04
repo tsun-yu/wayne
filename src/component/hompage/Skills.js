5387337d647c17e02a88fa6cc889b4d77d9e87f9
Wayne <tsun0227@yahoo.com.tw>
2022-02-04 16:54:19 +0800

    increase new skills and adjust the view

--- src/component/hompage/Skills.js
+++ src/component/hompage/Skills.js
@@ -11,10 +11,12 @@ import {
   SiYarn,
   SiGit,
   SiPostman,
-  SiPython,
+  SiReactrouter,
+  SiRedux,
+  SiGithub,
 } from "react-icons/si";
 
-import { IoGitBranch } from "react-icons/io5";
+import { IoGitBranch, IoLogoVue } from "react-icons/io5";
 import SkillItem from "./SkillsItem";
 
 function Skills(props) {
@@ -23,68 +25,97 @@ function Skills(props) {
       <div className="skills home-content">
         <div className="topic">SKILLS</div>
         <hr />
-        <div className="skills__cards">
-          <SkillItem
-            coverTitle="英語技能"
-            backItems={["多益成績:810"]}
-            id="en-skill"
-          />
-          <SkillItem
-            coverTitle="HTML & CSS"
-            backItems={[
-              <>
-                <SiHtml5 /> HTML5
-              </>,
-              <>
-                <SiCss3 /> CSS3
-              </>,
-              <>
-                <SiBootstrap /> Bootstrap
-              </>,
-              <>
-                <SiSass /> SCSS
-              </>,
-              "RWD 切版",
-            ]}
-          />
-          <SkillItem
-            coverTitle="JavaScript"
-            backItems={[
-              <>
-                <SiJavascript /> JavaScript
-              </>,
-              <>
-                <SiJquery /> jQuery
-              </>,
-              <>
-                <SiReact /> React
-              </>,
-              "XHR / AJAX / Fetch",
-              "RESTful API",
-            ]}
-          />
-          <SkillItem
-            coverTitle="Others"
-            backItems={[
-              <>
-                <SiNpm /> NPM, <SiYarn /> yarn 指令
-              </>,
-              <>
-                <SiGit /> Git 指令
-              </>,
-              <>
-                <IoGitBranch /> Git Flow and Branch
-              </>,
-              <>
-                <SiPostman /> Postman, JSON Server
-              </>,
-              <>
-                <SiPython /> Python
-                {/* <br />
-                (Numpy, Pandas) */}
-              </>,
-            ]}
-          />
+        <div className="skills__container">
+          <div className="skills__cards">
+            <SkillItem
+              coverTitle="英語技能"
+              backItems={["多益成績:810"]}
+              id="en-skill"
+            />
+            <SkillItem
+              coverTitle="HTML & CSS"
+              backItems={[
+                <>
+                  <SiHtml5 /> HTML5
+                </>,
+                <>
+                  <SiCss3 /> CSS3
+                </>,
+                "BEM 命名",
+                <>
+                  <SiBootstrap /> Bootstrap 4
+                </>,
+                <>
+                  <SiSass /> SCSS
+                </>,
+                "RWD 切版",
+              ]}
+            />
+            <SkillItem
+              coverTitle="JavaScript"
+              backItems={[
+                <>
+                  <SiJavascript /> JavaScript ES6
+                </>,
+                <>
+                  <SiJquery /> jQuery
+                </>,
+                "AJAX / Fetch",
+                "Promise / Async Await",
+                "RESTful API",
+              ]}
+            />
+            <SkillItem
+              coverTitle="React"
+              backItems={[
+                <>
+                  <SiReact /> React 17
+                </>,
+                <>
+                  <SiReactrouter /> React-router
+                </>,
+                <>
+                  <SiRedux /> Redux
+                </>,
+                "Functional component",
+                "React Hooks",
+              ]}
+            />
+            <SkillItem
+              coverTitle="Vue"
+              backItems={[
+                <>
+                  <IoLogoVue /> Vue 2
+                </>,
+                <>
+                  <IoLogoVue /> Vue 3
+                </>,
+                "Vue-router",
+                "Vuex",
+                "Vue CLI",
+              ]}
+            />
+            <SkillItem
+              coverTitle="Others"
+              backItems={[
+                <>
+                  <SiNpm /> NPM, <SiYarn /> yarn
+                </>,
+                <>
+                  <SiGit /> Git
+                </>,
+                <>
+                  <SiGithub /> Github
+                </>,
+                <>
+                  <IoGitBranch /> Git Flow and Branch
+                </>,
+                <>
+                  <SiPostman /> Postman
+                </>,
+              ]}
+            />
+          </div>
         </div>
       </div>
     </>
