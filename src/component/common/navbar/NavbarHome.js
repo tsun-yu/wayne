70c3d271b6b5486888df92ed88c7ed614b5b9bc7
tsun-yu <69957560+tsun-yu@users.noreply.github.com>
2024-02-06 13:53:50 +0800

    feat: mobile navbar adjustment

--- src/component/common/navbar/NavbarHome.js
+++ src/component/common/navbar/NavbarHome.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
 import { useHistory } from "react-router-dom";
 import Menu from "../Menu";
+import styled from "styled-components";
+import classNames from "classnames";
 
-function NavabarHome(props) {
-  const { resetColor, menuToggle } = props;
-  let history = useHistory();
+function NavabarHome() {
+  const [shrinkNav, setShrinkNav] = useState(false);
+  const navbarCLass = classNames({ navbar__sm: shrinkNav, navbar__home: true });
 
   const slr = (id) => document.querySelector(id);
 
@@ -24,9 +26,7 @@ function NavabarHome(props) {
       //縮小
       if (slr(".navbar__home") === null) return;
 
-      document.body.clientWidth >= 540 && window.scrollY >= 1
-        ? slr(".navbar__home").classList.add("navbar__sm")
-        : slr(".navbar__home").classList.remove("navbar__sm");
+      window.scrollY >= 1 ? setShrinkNav(true) : setShrinkNav(false);
 
       //網頁高度
       const documentHeight = document.body.offsetHeight;
@@ -40,20 +40,20 @@ function NavabarHome(props) {
       slr(".navbar__progress").style.width = progressPercent + "%";
 
       //變色
-      for (let i = homeContents.length - 1; i >= 0; i--) {
-        if (scrollHeight > homeContents[i]) {
-          resetColor();
-          document
-            .querySelectorAll(`.navbar__home > ul > li`)
-            [i].classList.add("navbar__selected");
-          break;
-        } else {
-          resetColor();
-        }
-      }
+      // for (let i = homeContents.length - 1; i >= 0; i--) {
+      //   if (scrollHeight > homeContents[i]) {
+      //     resetColor();
+      //     document
+      //       .querySelectorAll(`.navbar__home > ul > li`)
+      //       [i].classList.add("navbar__selected");
+      //     break;
+      //   } else {
+      //     resetColor();
+      //   }
+      // }
     });
 
-    slr("#menu-home").classList.add("menu__selected");
+    // slr("#menu-home").classList.add("menu__selected");
     return () => {};
   }, []);
 
@@ -67,53 +67,76 @@ function NavabarHome(props) {
 
   return (
     <>
-      <header className="navbar__home navbar">
+      <NavBar className={navbarCLass}>
         <div className="navbar__progress navbar__progress--home"></div>
         <span>WAYNE</span>
-        <ul
-          className="navbarHam"
-          onClick={(e) => {
-            slr(".menu").classList.contains("menu__appear")
-              ? menuToggle("out")
-              : menuToggle("in");
-          }}
-        >
-          <li id="hamBar1"></li>
-          <li id="hamBar2"></li>
-          <li id="hamBar3"></li>
-        </ul>
-        <ul className="navbar__options">
-          {/* <li
-            id="nav-about"
-            onClick={() => {
-              scrollTo(".about-section");
-            }}
-          >
-            ABOUT
-          </li>
-          <li
-            id="nav-skill"
-            onClick={() => {
-              scrollTo(".skills");
-            }}
-          >
-            SKILLS
-          </li>
-          <li
-            id="nav-exp"
-            onClick={() => {
-              scrollTo(".exp__sec");
-            }}
-          >
-            EXPERIENCE
-          </li>
-          <li onClick={() => history.push("/portfolios")}>PORTFOLIOS</li>
-          <li onClick={() => history.push("/contact")}>CONTACT</li> */}
-        </ul>
-      </header>
-      <Menu menuToggle={menuToggle} />
+      </NavBar>
     </>
   );
 }
+
+const NavBar = styled.header`
+  width: 100vw;
+  height: 6.25rem;
+  transition: 0.5s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 20;
+
+  &.navbar__sm {
+    height: 3.125rem;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.5);
+    backdrop-filter: blur(2px);
+    transition: 0.5s;
+    box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.1);
+
+    span {
+      display: inline-block;
+      font-family: "Pacifico", cursive;
+      font-size: 1.5rem;
+    }
+    #logo {
+      cursor: pointer;
+    }
+  }
+
+  span {
+    display: none;
+  }
+
+  .navbar__progress {
+    position: absolute;
+    backdrop-filter: blur(5px);
+    height: 100%;
+    z-index: -2;
+    left: 0;
+    background-color: rgba(253, 195, 0, 0.5);
+  }
+
+  @media (max-width: 540px) {
+    &.navbar__sm {
+      justify-content: space-between;
+      background-color: rgba(255, 255, 255, 1);
+
+      span {
+        display: inline-block;
+        font-family: "Pacifico", cursive;
+        font-size: 1.5rem;
+        margin-left: 1rem;
+      }
+      #logo {
+        cursor: pointer;
+      }
+    }
+
+    .navbar__progress {
+      background-color: #fdc300;
+      box-shadow: none;
+    }
+  }
+`;
 
 export default NavabarHome;
